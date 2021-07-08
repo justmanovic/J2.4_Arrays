b247d7bb53df040208932cbe1fa84fae6159c541
jonathan justman <jojo.justman@gmail.com>
2021-07-09 00:06:27 +0200

    clean + menu

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -1,81 +1,117 @@
 journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 
-journalists_length = Array.new
+journalists_length = Array.new # On crée un tableau afin de stocker les lengths de chaque pseudo, les uns après les autres
 
-journalists.each do |element|
-  journalists_length.push(element.length-1)
+journalists.each do |el| # pour chaque élément
+    journalists_length.push(el.length-1) # on ajoute la length de l'élément au tableau en dernière position... 
 end
 
-print journalists_length
+puts "Tapez 1 : Combien y a-t-il de handle dans cette array ?"
+puts "Tapez 2 : Quel est le handle le plus court de cette liste ?"
+puts "Tapez 3 : Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)"
+puts "Tapez 4 : Combien commencent par une majuscule (première lettre juste après le @) ?"
+puts "Tapez 5 : Trie la liste de handle par ordre alphabétique."
+puts "Tapez 6 : Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)"
+puts "Tapez 7 : Quelle est la position dans l'array de la personne @epenser ?"
+puts "Tapez 8 : Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)"
+puts
+puts
 
-min = journalists[0].length
-handle_min = min
+print ">> "
+user_choice = gets.chomp.to_i
+puts
+puts
 
-journalists.each do |element|
-    length_current_el = element.length
-    if length_current_el < min
-        min = length_current_el
+if user_choice ==1
+    puts
+    puts ">>>>>  il y a #{journalists.length} handles dans cet array ! <<<<"
+    puts
+
+elsif user_choice == 2
+
+    # print journalists_length
+
+    min = journalists[0].length # à l'initialisation, le pseudo le plus court contient le nb de caractères du 1er élément du tableau
+    handle_min = journalists[0]  # à l'initialisation, le pseudo le plus court = le 1er pseudo
+
+    journalists.each do |element|  # on parcourt le tableau des handles
+        length_current_el = element.length # on enregistre la length chaque élément
+        if length_current_el < min # si la length de l'élément en question est < à la min length...
+            min = length_current_el  # ...celle-ci devient la min-length
+        end
+        if element.length == min # si la length de l'élément en question est la min length
+        handle_min = element # cela veut dire que l'élément en question est celui qui a la min length
+        end
     end
-    if element.length == min
-      handle_min = element
+    puts
+    puts
+    puts ">>>>>  Le handle le plus court mesure #{min} caractères <<<<"
+    puts ">>>>>  Il s'agit du journaliste surnommé #{handle_min} <<<<"
+
+
+puts
+puts
+
+elsif user_choice == 3
+
+    compteur_5_car = 0 # initialisation... au départ, le compteur de handle mesurant 5 caractères est à 0
+
+    journalists.each do |element| # pour chaque élément du tableau journalists
+
+        if (element.length == 6) # si l'élément en question contient 6 caractères (en incluant le @, donc 5 en le retirant)
+            compteur_5_car = compteur_5_car + 1 # le compteur s'incrémente
+        end
+    end
+
+    puts ">>>>>  Il y a #{compteur_5_car} handles contenant 5 caractères <<<<"
+
+puts
+puts
+
+
+elsif user_choice == 4
+
+compteur_maj = 0 # initialisation... le compteur des éléments qui commencent par une maj est à 0
+
+journalists.each do |element| # on parcourt le tableau
+    if element[1] =~ /[A-Z]/ # si le 2eme caractère (le 1er après le "@"") de l'élément en question contient une lettre comprise entre A et Z (en maj)
+        compteur_maj  = compteur_maj  +1 # le compteur de maj s'incrémente (même raisonnement que question précédente)
     end
 end
-puts "Le handle le plus court mesure #{min} caractères"
-puts "Il s'agit du journaliste surnommé #{handle_min}"
 
+puts ">>>>>  Il y a #{compteur_maj} éléments qui commencent par une majuscule <<<<"
 
+puts
+puts
 
-journalists.each do |element|
+elsif user_choice == 5
 
-end
+puts ">>>>>  Liste triée par ordre alphabétique <<<<"
+puts
 
-# Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
-
-compteur = 0
-
-journalists.each do |element|
-    # print element
-    if (element.length == 6)
-        compteur = compteur + 1
-    end
-end
-
-puts "Il y a #{compteur} handles contenant 5 caractères"
-
-
-# Combien commencent par une majuscule (première lettre juste après le @) ?
-
-compteur_up=0
-
-journalists.each do |element|
-    if element[1] =~ /[A-Z]/
-        compteur_up = compteur_up +1
-    end
-end
-
-puts "Il y a #{compteur_up} éléments qui commencent par une majuscule"
-
-
-print journalists.sort
-puts "-------------"
+print journalists.sort # tri par ordre alphabétique et affichage
+puts 
+puts
 # journalists = journalists.sort
 # print journalists
 
+elsif user_choice == 6
 
-print journalists.sort_by(&:length)
-puts "---------------------------------------------------------"
+puts ">>>>>  Liste triée taille des handles <<<<"
+puts
 
+print journalists.sort_by(&:length) # tri du tableau selon leur nombre de caractères et affichage
+puts 
+puts
 
-# journalists.each do |element|
-#   element = element[1]
-# end
+elsif user_choice == 7
 
-# print journalists
+puts ">>>>>  Le handle @epenser est en #{journalists.index("@epenser")}ème position dans la liste <<<<"
 
-puts "la position de @epenser est #{journalists.index("@epenser")}"
+puts
+puts
 
-
-puts "---------------------------------------------------------"
+elsif user_choice == 8
 
 
 # METHODE 1
@@ -107,23 +143,58 @@ puts "---------------------------------------------------------"
 
 # METHODE 2
 
-for i in 0...journalists.length
-    compteur = 0
+journalists_length_sorted = journalists_length.sort # On trie la liste qui contient les length de chaque élément par ordre croissant, on en fait un nouveau tableau
+
+occurences = [] # On crée un tableau qui va contenir le nb d'occurences de chaque length
+
+journalists_length_sorted.each do |el| 
+ occurences.push "#{el} apparait #{journalists_length_sorted.count(el)} fois, soit #{journalists_length_sorted.count(el)*100.00/journalists_length_sorted.length}% des fois"
 end
 
 
-
-
-print
+puts occurences
 
     # tab_compteur.push()
 
-i=0
 
-puts ""
+
+
+
+puts 
+puts
+puts
+
+
+
+=begin
+
+cette solution marche avec la METHODE 1 (voir plus haut)
+
+i=0
 while i<tab_compteur.length
     if(tab_compteur[i]!=0)
     puts "il y a #{tab_compteur[i]} handles de #{i} caractères, soit #{tab_compteur[i] /journalists_length.length.to_f * 100.00}%"
     end
     i+=1
+end
+
+=end
+
+puts 
+puts
+puts
+
+else 
+    puts "Mauvais choix... Tant pis pour toi ! "
+    puts
+    puts
+    sleep(0.5)
+    puts "Tout le contenu de votre ordinateur va être supprimé"
+
+    300.times do
+        print "."
+        sleep(0.01)
+    end
+    puts
+    puts
 end
